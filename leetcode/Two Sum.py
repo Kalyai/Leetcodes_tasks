a6a808a51a712f7c2d9c80a77c5bf8a73c6f739f
Kalyaev Sergei <141425017+Kalyai@users.noreply.github.com>
2024-03-21 21:34:34 +0300

    Two Sum.py
    Just a few better code

--- leetcode/Two Sum.py
+++ leetcode/Two Sum.py
@@ -1,14 +1,9 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        len_nums = len(nums)
-        serch = dict()
-
-        for i in range(len_nums):
-            x = nums[i]
-
-            if (target - x) in serch and serch is not None:
-                return [serch[target - x], i]
-            serch[x] = i
-
-res = Solution().twoSum([3, 3], 6)
-print(res)
+        cash = dict()
+        for i in range(len(nums)):
+            if target - nums[i] in cash:
+                return [cash[target - nums[i]], i]
+            
+            if nums[i] not in cash:
+                cash[nums[i]] = i
