f679c5ea15fb923824cd1e20087b01bbbf5c5a95
Kalyaev Sergei <141425017+Kalyai@users.noreply.github.com>
2024-03-27 18:50:00 +0300

    Backtracking

--- leetcode/Medium/22. Generate Parentheses.py
+++ leetcode/Medium/22. Generate Parentheses.py
@@ -1,14 +1,16 @@
 class Solution(object):
     def generateParenthesis(self, n):
-        def make(s = '', left = 0, right = 0):
-            if len(s) == 2 * n:
-                result.append(s)
-                return
-            if left < n:
-                make(s + '(', left + 1, right)
-            if right < left:
-                make(s + ')', left, right + 1)
+        parentheses = list()
 
-        result = []
-        make()
-        return result
+        def main(string, n, left, right):
+            if len(string) == 2 * n:
+                parentheses.append(string)
+                return
+
+            if left < n:
+                main(string + '(', n, left + 1, right)
+            if left > right:
+                main(string + ')', n, left, right + 1)
+
+        main('', n, 0, 0)
+        return parentheses
