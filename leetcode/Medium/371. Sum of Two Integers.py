a8e63e0e1346c0cbe88533a222800b0336626585
Kalyaev Sergei <141425017+Kalyai@users.noreply.github.com>
2024-03-28 12:42:55 +0300

    Bit manipulation

---
+++ leetcode/Medium/371. Sum of Two Integers.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def getSum(self, a, b):
+        mask = 0xffffffff
+
+        while (b & mask) > 0:
+            carry = ( a & b ) << 1
+            a = (a ^ b) 
+            b = carry
+
+        return (a & mask) if b > 0 else a
+        
