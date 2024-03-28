d8e59a6835d6ff7b99cea800d87eb7f7d8230d0b
Kalyaev Sergei <141425017+Kalyai@users.noreply.github.com>
2024-03-28 18:03:55 +0300

    Delete leetcode/Contains Duplicate.py

--- leetcode/Contains Duplicate.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        hash = set()
-        for n in nums:
-            if n in hash:
-                return True
-            hash.add(n)
-        return False
