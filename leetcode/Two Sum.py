e8e4587ff27bccff0355a4613100bf944f56c2e4
Kalyaev Sergei <141425017+Kalyai@users.noreply.github.com>
2024-03-28 18:06:25 +0300

    Delete leetcode/Two Sum.py

--- leetcode/Two Sum.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        cash = dict()
-        for i in range(len(nums)):
-            if target - nums[i] in cash:
-                return [cash[target - nums[i]], i]
-            
-            if nums[i] not in cash:
-                cash[nums[i]] = i
