5c82f2e8684c4ae289d7d2fd9e8366e42ac88558
Kalyaev Sergei <141425017+Kalyai@users.noreply.github.com>
2024-03-28 18:06:22 +0300

    Delete leetcode/Top K Frequent Elements.py

--- leetcode/Top K Frequent Elements.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        cash = dict()
-        for n in nums:
-            if n not in cash:
-                cash[n] = 0
-            cash[n] += 1
-
-        return sorted(cash, key=lambda x: cash[x], reverse=True)[:k]
