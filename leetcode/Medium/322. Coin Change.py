faef800631d804eed649d10f140a24688719b418
Kalyaev Sergei <141425017+Kalyai@users.noreply.github.com>
2024-04-08 14:16:35 +0300

    DP

--- leetcode/Medium/322. Coin Change.py
+++ leetcode/Medium/322. Coin Change.py
@@ -1,11 +1,11 @@
 class Solution(object):
     def coinChange(self, coins, amount):
-            dp = [amount + 1] * (amount + 1)
-            dp[0] = 0
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
 
-            for i in range(1, len(dp)):
-                for c in coins:
-                    if i >= c:
-                        dp[i] = min(dp[i], 1 + dp[i - c])
+        for i in range(1, amount + 1):
+            for coin in coins:
+                if i >= coin:
+                    dp[i] = min(1 + dp[i - coin], dp[i])
 
-            return dp[amount] if dp[amount] != amount + 1 else -1
+        return dp[-1] if dp[-1] != amount + 1 else -1
