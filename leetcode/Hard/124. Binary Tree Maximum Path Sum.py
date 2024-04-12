a4b26ac884b9aba2cd0f9d61c54d2f948ed3d437
Kalyaev Sergei <141425017+Kalyai@users.noreply.github.com>
2024-04-12 11:22:48 +0300

    Binary Tree

--- leetcode/Hard/124. Binary Tree Maximum Path Sum.py
+++ leetcode/Hard/124. Binary Tree Maximum Path Sum.py
@@ -1,22 +1,21 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
 class Solution(object):
-    answer = -1000
     def maxPathSum(self, root):
-        self.dfs(root)
-        res = Solution.answer
-        Solution.answer = -1000
-        return res
+        def helper(root):
+            if root is None:
+                return
+            
+            left_max = max(helper(root.left), 0)
+            right_max = max(helper(root.right), 0)
+            result[0] = max(result[0], left_max + right_max + root.val)
 
-    def dfs(self, node):
-        if node is None:
-            return 0
+            return max(left_max, right_max) + root.val
 
-        max_right_node = max(self.dfs(node.right), 0)
-        max_left_node = max(self.dfs(node.left), 0)
-        Solution.answer = max(Solution.answer, max_right_node + max_left_node + node.val)
-        return max(max_right_node, max_left_node) + node.val
+        result = [-1000]
+        helper(root)
+        return result[0]
